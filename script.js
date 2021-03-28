8cf73f3d25c8f2c6bca6a726c63890439d8442f7
julieli1 <missvckise@gmail.com>
2021-03-28 01:34:20 +0000

    javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+//everything is set to 0
+let b1 = 0
+let b2 = 0
+let height = 0
+let area = 0
+
+document.getElementById('button').addEventListener('click', math)
+
+function math () {
+  b1 = document.getElementById('base1').value
+  b1 = parseInt(b1)
+  b2 = document.getElementById('base2').value
+  b2 = parseInt(b2)
+  height = document.getElementById('height').value
+  height = parseInt(height)
+  area = (b1 + b2) / 2 * height
+  alert(area)
+}
