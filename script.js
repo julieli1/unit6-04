40fd2101b6f2748dace11d61614560f286754531
julieli1 <missvckise@gmail.com>
2021-03-28 01:39:36 +0000

    syntax

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-//everything is set to 0
+// everything is set to 0
 let b1 = 0
 let b2 = 0
 let height = 0
 let area = 0
 
+// when the button is clicked, the math calculate
 document.getElementById('button').addEventListener('click', math)
 
+// this is how to solve the area of a trapezoid
 function math () {
   b1 = document.getElementById('base1').value
   b1 = parseInt(b1)
